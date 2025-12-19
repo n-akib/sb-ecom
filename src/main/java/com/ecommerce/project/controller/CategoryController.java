1c857f4a9c046a030e5507870fa3cd2b61bd893e
n-akib <n.mahfuze@gmail.com>
2025-12-19 14:30:56 +0600

    Add OpenAPI Documentation(Swagger)

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,8 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/categories")
+@Tag(name = "Category Management", description = "APIs for managing categories")
 public class CategoryController {
 
     private CategoryService categoryService;
@@ -18,18 +21,18 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("api/public/categories")
+    @GetMapping("/categories")
     public List<Category> getAllCategories() {
         return categoryService.getAllCategories();
     }
 
-    @PostMapping("api/public/categories")
+    @PostMapping("/categories")
     public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "Category created successfully";
     }
 
-    @DeleteMapping("api/public/categories/{categoryId}")
+    @DeleteMapping("/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
 
         try {
--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Product;
 import com.ecommerce.project.service.ProductService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,25 +11,28 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/products")
+@Tag(name = "Product Management", description = "APIs for managing products")
 public class ProductController {
+
     private ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
-    @GetMapping("api/public/products")
+    @GetMapping("/products")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
-    @PostMapping("api/public/products")
+    @PostMapping("/products")
     public String createProduct(@RequestBody Product product) {
         productService.createProduct(product);
         return "Product created successfully";
     }
 
-    @DeleteMapping("api/public/products/{productId}")
+    @DeleteMapping("/products/{productId}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long productId) {
         try {
             String status = productService.deleteProduct(productId);
